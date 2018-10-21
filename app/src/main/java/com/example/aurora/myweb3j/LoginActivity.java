6861c045985f0cd0b6b090590d4cc99221dc4cec
alistairzhu <alistairzhu@gmail.com>
2018-10-21 12:07:07 -0400

    remove empty line

--- app/src/main/java/com/example/aurora/myweb3j/LoginActivity.java
+++ app/src/main/java/com/example/aurora/myweb3j/LoginActivity.java
@@ -31,7 +31,6 @@ public class LoginActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         String TAG = "Return";
 
         //connect to the ethereum client node
